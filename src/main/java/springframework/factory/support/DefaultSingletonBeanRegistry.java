f81942025cc98492a007786c27f56d3e20c72d8c
xpengkang <xpengkang@outlook.com>
2022-01-12 00:08:59 +0800

    完善实例化操作，增加 InstantiationStrategy 实例化策略接口，并新增了两个实例化类1

--- src/main/java/springframework/factory/support/DefaultSingletonBeanRegistry.java
+++ src/main/java/springframework/factory/support/DefaultSingletonBeanRegistry.java
@@ -7,9 +7,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
  */
 public class DefaultSingletonBeanRegistry implements SingletonBeanRegistry {
 
